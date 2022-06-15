ed60970875c97a8a3c4da276c07329415bfcac49
elhaddaji_Otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2022-06-15 20:10:36 +0100

    Create footer.php

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright <?php echo date("Y",time()) ; ?> <a href="http://elhaddaji-otmane.netlify.app/">Otmane</a></strong>
+</footer>
